6fa828cedeac41d66a691e20cb7b2568be99e5bc
mohamedyasser2206705 <mohamed2206705@miuegypt.edu.eg>
2024-06-23 20:11:33 +0300

    my part again

---
+++ models/service.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const serviceSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Service = mongoose.model("Service", serviceSchema);
+
+export default Service;
